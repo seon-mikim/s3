4a4b2f0efb9019f77a0942bcd9d4026169b0b295
seon-mikim <tjsalsun@gmail.com>
2024-02-23 21:55:46 +0900

    feat:cros 에러로 인한 수정

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import  express, { Request, Response } from "express"
 import { upload } from "./uploadS3";
+import cors from 'cors';
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }),
+  )
 
 app.post('/upload', upload.single('img'), (req: Request, res: Response) => {
     res.json(req.file)
